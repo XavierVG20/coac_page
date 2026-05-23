4f93b714522392e2101fa3be3e0205a508593a0b
¨XavierVG20¨ <kjaviervelasque16@gmail.com>
2026-05-23 15:59:00 -0500

    correccion footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -85,7 +85,7 @@
 <div class="container-fluid text-white" style="background: #061429;">
     <div class="container text-center">
         <div class="row justify-content-end">
-            <div class="col-lg-8 col-md-6">
+            <div class="col-lg-12 col-md-6">
                 <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
                     <p class="mb-0">
                         &copy; 2025 <strong>Cooperativa Pujilí</strong> | Todos los derechos reservados
